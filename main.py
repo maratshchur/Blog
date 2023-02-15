7aefa3ef39d5b8c0b83585f688b0b1a7ef6659d2
maratshchur <112699122+maratshchur@users.noreply.github.com>
2023-02-15 13:16:14 +0300

    Blog

---
+++ main.py
@@ -0,0 +1,66 @@
+import redis
+import datetime as dt
+from fastapi import FastAPI
+
+app = FastAPI()
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+@app.post("/create")
+def create_blog_post(theme: str, title: str, content: str, author: str):
+    # генерируем id для поста
+    post_id = r.incr("blog:current_post_id")
+    time = dt.datetime.now()
+    last_change_time = time.strftime("%m/%d/%Y , %H:%M:%S")
+    # сохраняем данные поста в Redis
+    try:
+        r.hset(post_id, "Theme", theme)
+        r.hset(post_id, "Title", title)
+        r.hset(post_id, "Content", content)
+        r.hset(post_id, "Author", author)
+        r.hset(post_id, "Changed", last_change_time)
+        return {"message": "Post created successfully", "post_id": post_id}
+    except redis.exceptions.RedisError:
+
+        r.decr("blog:current_post_id")
+        return {"success": False, "error": "Failed to create post"}
+
+
+@app.put("/update/{post_id}")
+def update_blog_post(post_id: str, theme: str, title: str, content: str, author: str):
+    # обновляем данные поста в Redis
+    post = r.hgetall(post_id)
+    if post:
+        time = dt.datetime.now()
+        last_change_time = time.strftime("%m/%d/%Y , %H:%M:%S")
+        r.hset(post_id, "Theme", theme)
+        r.hset(post_id, "Title", title)
+        r.hset(post_id, "Content", content)
+        r.hset(post_id, "Author", author)
+        r.hset(post_id, "Author", last_change_time)
+
+        return {"message": "Post updated successfully"}
+    else:
+        return {"message": "Post not found"}
+
+
+@app.get("/view/{post_id}")
+def view_blog_post(post_id: str):
+    # получаем данные поста из Redis
+    post = r.hgetall(post_id)
+    if post:
+        return post
+    else:
+        return {"message": "Post not found"}
+
+
+@app.delete("/delete/{post_id}")
+def delete_blog_post(post_id: str):
+    # удаляем пост из Redis
+    post = r.hgetall(post_id)
+    if post:
+        r.delete(post_id)
+        return {"status": "success"}
+    else:
+        return {"message": "Post not found"}
